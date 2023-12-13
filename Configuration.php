4160d61dee21ef022790e68df0747dc4a9278daa
ljacqu <ljacqu@users.noreply.github.com>
2023-12-13 12:10:58 +0100

    Add Twitch client configs

--- Configuration.php
+++ Configuration.php
@@ -17,6 +17,16 @@ class Configuration {
   /** Database password. */
   const DB_PASS = '';
 
+
+  // ------
+  // Twitch application details
+  // ------
+  // Register a client at https://dev.twitch.tv/console/apps
+
+  const TWITCH_CLIENT_ID = '';
+  const TWITCH_CLIENT_SECRET = '';
+
+
   private function __construct() {
   }
 }
