85c760fc3ee81365460bce6d0c69defe73774a70
ljacqu <ljacqu@users.noreply.github.com>
2023-12-14 16:56:11 +0100

    Add more validation hints to ratings field / allow "3." as input

--- assets/editrating.js
+++ assets/editrating.js
@@ -1,16 +1,19 @@
 function handleRating(inputElem) {
-    const inputValue = inputElem.value;
+    const inputValue = inputElem.value.trim();
     let rating;
-    if (/^[1-5]$/.test(inputValue)) {
-        rating = inputValue + '.0';
+    if (/^[1-5]\.?$/.test(inputValue)) {
+        const decimalSuffix = inputValue.slice(-1) === '.' ? '0' : '.0';
+        rating = inputValue + decimalSuffix;
     } else if (/^([1-4]\.\d)|(5\.0)$/.test(inputValue)) {
         rating = inputValue;
     } else {
         inputElem.className = 'ratingerror';
+        inputElem.title = 'Please use a rating from 1.0 to 5.0';
         return;
     }
 
     inputElem.disabled = true;
+    inputElem.title = '';
 
     const formData = new FormData();
     formData.append('rating', rating);
